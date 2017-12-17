f038fa888ee52d622f15e1fb83f34636d996675f
ulises-jeremias <ulisescf.24@gmail.com>
2017-12-17 12:14:38 -0300

    Para fede

---
+++ Tercer Año/Segundo Semestre/Programacion Concurrente/Parciales/2013/1raFechaTema1_1.cpp
@@ -0,0 +1,31 @@
+sem habilitador[1..10] = ([10], 0);
+sem trabajar[1..10] = ([10], 0);
+sem finalizado[1..10] = ([10], 0);
+int resultado;
+
+process Central
+{
+        int i, e;
+
+        for (i = 0; i < 10; i++) {
+                p(habilitador[i]);
+        }
+
+        while (true) {
+                e = TrabajarEnServidor();
+                v(trabajar[e]);
+                p(finalizado[e]);
+                printf("%d\n", resultado);
+        }
+}
+
+process Sede[int i = 1..10]
+{
+        v(habilitador[i]);
+
+        while (true) {
+                p(trabajar[i]);
+                resultado = trabajar();
+                v(finalizado[i]);
+        }
+}
