25863664cbb8e546722caad49055733885f6c2be
ulises-jeremias <ulisescf.24@gmail.com>
2017-12-17 12:28:30 -0300

    Solved

---
+++ Tercer Año/Segundo Semestre/Programacion Concurrente/Parciales/2013/1raFechaTema1_2.cpp
@@ -0,0 +1,17 @@
+process Servidor
+{
+        int cantidad = 10;
+
+        do (cantidad; Usuario[*]?solicitarEntrar()) {
+                cantidad--;
+        } [](Usuario[*]?finalizar()) {
+                cantidad++;
+        }
+}
+
+process Usuario[int i = 1..N]
+{
+        Servidor!solicitarEntrar();
+        // trabajando
+        Servidor!finalizar();
+}
