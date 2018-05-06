1f097ababfe474bc2fc38f561b78eb223a3be604
ulises-jeremias <ulisescf.24@gmail.com>
2018-05-05 21:29:46 -0300

    Added practica3

---
+++ Cuarto Año/Sistemas Paralelos/Practicas/Practica 3/Fuentes/ejercicio2.c
@@ -0,0 +1,25 @@
+//Ejercicio 2
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <math.h>
+
+
+int main(int argc,char*argv[]){
+        double x,scale;
+        int i;
+        int numThreads = atoi(argv[2]);
+        int N=atoi(argv[1]);
+        omp_set_num_threads(numThreads);
+        scale=2.78;
+        x=0.0;
+
+ #pragma omp parallel for
+        for(i=1; i<=N; i++) {
+                x= x + sqrt(i*scale) + 2*x;
+        }
+
+        printf("\n Resultado: %f \n",x);
+
+        return(0);
+}
