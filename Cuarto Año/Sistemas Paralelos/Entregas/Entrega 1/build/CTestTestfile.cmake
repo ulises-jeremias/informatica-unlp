fcf003d32e75d227708b365c9f59b03840959dca
ulises-jeremias <ulisescf.24@gmail.com>
2018-05-12 18:26:54 -0300

    Added cmake

--- Cuarto Año/Sistemas Paralelos/Entregas/Entrega 1/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ulises-jeremias/Projects/Github/Facultad-de-Informatica/Cuarto Año/Sistemas Paralelos/Entregas/Entrega 1
-# Build directory: /home/ulises-jeremias/Projects/Github/Facultad-de-Informatica/Cuarto Año/Sistemas Paralelos/Entregas/Entrega 1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("ejercicio1")
-subdirs("ejercicio2")
